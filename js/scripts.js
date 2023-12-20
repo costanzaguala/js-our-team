8309962a401be30bf80e347eeadff94eaabee06c
costanzaguala <costanzasilviaguala@gmail.com>
2023-12-20 22:51:58 +0100

    milestone 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,33 @@
-/* 
-Chiedere all'utente una parole
-creare una funziona per capire se la parola e' palindroma
-*/
+const teamMembers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+];
 
-const wordUser = prompt('Inserisci una parola');
-console.log('wordUser', wordUser, typeof wordUser);
-
-const worldResult = wordPalindrome(wordUser);
-
-if (worldResult == true) {
-    alert(' è Palindroma')
-}
-
-else {
-    alert(' non è Palindroma')
-}
-
-// FUNZIONE
-
-function wordPalindrome(word) {
-
-let wordReverse = '';
-
-for (let i = word.length - 1; i >= 0; i--) {
-
-    wordReverse += word[i];
-}
-
-console.log ('wordReverse', wordReverse, typeof wordReverse);
-
-if (word == wordReverse) {
-
-    return true;
-}
-
-else {
-
-    return false;
-
-}
-
-}
